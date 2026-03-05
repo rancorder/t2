1ac50221e55c260f3cb0fe8a708254a82c07faa7
hideki <xzengbu@gmail.com>
2026-03-05 15:25:01 +0900

    Delete main.tsx

--- main.tsx
+++
@@ -1,5 +0,0 @@
-*,*::before,*::after{box-sizing:border-box;margin:0;padding:0}
-html,body,#root{width:100%;height:100%;overflow:hidden}
-::selection{background:#e8000f;color:#000}
-*{scrollbar-width:none}
-::-webkit-scrollbar{display:none}
